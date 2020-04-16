bb1fe7ecdb9e213abe6947006c5b9b558c841ee4
conan2613-dev <63732893+conan2613-dev@users.noreply.github.com>
2020-04-16 20:00:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
 const express = require('express');
 const articleRouter = require('./routes/article')
-const functions = require('firebase-functions');
+//const functions = require('firebase-functions');
 const mongoose = require('mongoose');
 const Articles = require('./models/articles');
-const firebase = require('firebase')
+//const firebase = require('firebase')
 
 //MLABS MONGO DB
 mongoose.connect('mongodb+srv://sagar:sagar12345@myfirstmongodb-tldsv.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 //FOR FIREBASE
-const serviceAccount = require('./nodejs2613-firebase-adminsdk-7cds5-5462fc2575.json')
 
-const admin = require("firebase-admin");
-firebase.initializeApp({
-    serviceAccount: "./nodejs2613-firebase-adminsdk-7cds5-5462fc2575.json",
 
-    databaseURL: "https://nodejs2613.firebaseio.com"
-}, );
-
-admin.initializeApp(
-    functions.config.admin
-)
-
-//FOR FIREBASE REALTIME DATABASE
-var db = firebase.database();
-var ref = db.ref("/user_data");
 
 
 
@@ -50,22 +36,9 @@ app.get('/', async(req, res) => {
         // res.send("hello this is index page")
 
     //SENDING DATA TO FIREBASE REALTIME DATABASE
-    ref.set([{
-            id: 20,
-            name: "Jane Doe",
-            email: "jane@doe.com",
-            website: "https://jane.foo.bar"
-        },
-        {
-            id: 21,
-            name: "John doe",
-            email: "john@doe.com",
-            website: "https://foo.bar"
-        }
-    ]);
+    
 
 })
 
-exports.app = functions.https.onRequest(app, () => {
-    console.log("working");
+
 });
